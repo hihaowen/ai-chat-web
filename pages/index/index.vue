830a62fb096963f71fdc2e5567ca9b01ccee92fc
haowenzhi <haowenzhi@dfs800.com>
2023-06-02 18:53:20 +0800

    Add 敬请期待pdf

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -16,6 +16,15 @@
 			<uni-card title="智能表单" sub-title="与 Ai 完成一问一答形式的表单，快速获取答案。" extra="去提问 >" thumbnail="/static/app-icon.png"
 				@click="formChat">
 			</uni-card>
+
+			<uni-card title="PDF对话" sub-title="就像聊天一样，轻松提取和理解PDF内容，随需生成个性化PDF。" extra="去对话 >"
+				thumbnail="/static/pdf-icon.png" @click="pdfChat">
+				<text class="uni-body">擅长：深度解析和定制生成PDF文件。
+					能力：从多种PDF格式提取并理解内容，将对话保存为PDF。
+					技术：结合文本解析和大语言模型，处理复杂的PDF文件。
+					优点：自动化处理PDF以提升效率，深度理解内容，界面简洁易用。
+				</text>
+			</uni-card>
 		</view>
 	</view>
 </template>
@@ -33,6 +42,12 @@
 					url: `/pages/index/app`
 				})
 			},
+			pdfChat(e) {
+				uni.showToast({
+					title: "敬请期待",
+					icon: "none",
+				});
+			},
 			buyMember(e) {
 				uni.navigateTo({
 					url: '/pages/sso/member'
@@ -46,6 +61,6 @@
 	.home {}
 
 	.content {
-		padding: 20rpx 20rpx 0 20rpx;
+		padding: 20rpx 20rpx 20rpx 20rpx;
 	}
 </style>
